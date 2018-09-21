fb4fc219fcfe97ba996b31f3840bddbed15ec6be
Richard Yentes <ryentes@gmail.com>
2018-09-21 14:07:48 -0400

    adding maxLookup

---
+++ R/maxLookup.R
@@ -0,0 +1,15 @@
+#' A function for finding y of x
+#' 
+#' This functiont takes a two column data frame as input. It
+#' computes the max of the second column, and then returns the first
+#' corresponding value from the first column
+#' 
+#' @author Richard D. Yentes \email{rdyentes@ncsu.edu}
+#' @param x an nx2 data frame
+#' @export
+maxLookup <- function(x) {
+  max <- max(x[,2])
+  isMax <- which(x[,2] == max)
+  if (length(isMax) > 1) { return(x[isMax,1])}
+  else {return(x[isMax,1])}
+}
