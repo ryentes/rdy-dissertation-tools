85a03a9613244ec9124bfc9acf08188b19e3fd34
Richard Yentes <ryentes@gmail.com>
2018-10-01 17:42:56 -0400

    documented rmfiles

--- R/clearExperiment.R
+++ R/clearExperiment.R
@@ -5,6 +5,7 @@
 #' @param hypothesis specifies the research question or hypothesis
 #' @export
 clearExperiment <- function(hypothesis) {
+  rmfiles(glue::glue('logs/{hypothesis}/'))
   rmfiles(glue::glue('artifacts/{hypothesis}/ls/'))
   rmfiles(glue::glue('artifacts/{hypothesis}/eo/'))
   rmfiles(glue::glue('artifacts/{hypothesis}/md/'))
