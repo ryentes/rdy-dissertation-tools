4eeb69c1007a8cd11e90e48ac39e7d74f220f2c3
Richard Yentes <ryentes@gmail.com>
2019-04-20 19:45:51 -0400

    Create jLookup.r

---
+++ R/jLookup.r
@@ -0,0 +1,16 @@
+#' A function for finding y of x
+#' 
+#' This functiont takes a two column data frame as input. It
+#' computes the max of the second column, and the informedness corresponding
+#' to i
+#' 
+#' @author Richard D. Yentes \email{rdyentes@ncsu.edu}
+#' @param x an nx2 data frame
+#' @param i index at which to report informedness
+#' @export
+jLookup <- function(x, i) {
+  
+  isI <- which(x[,2] == i)
+  return(x[i,2])
+  }
+}
